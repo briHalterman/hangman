7d71492b929207cb4e0de4ad207ab14daa4c2fb6
Brittany Halterman <halterma@gmail.com>
2025-05-05 10:06:51 -0400

    Handle guess logic
    - Validates guess
    - Tracks correct and incorrect guesses
    - Counts strikes

--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,7 @@
 class Game
   def initialize
     @secret_word = SecretWordSelector.new.select_secret_word
-    @strikes = 0
+    @strike_counter = 0
     @correct_guesses = []
     @incorrect_guesses = []
   end
@@ -24,5 +24,25 @@ class Game
   def get_player_guess
     puts "Guess a letter:"
     gets.chomp.downcase
+  end
+
+  def validate_guess(guess)
+    guesses = @correct_guesses + @incorrect_guesses
+
+    if guess.length != 1 || !guess.match?(/^[a-z]$/) || guesses.include?(guess)
+      puts "Please choose exactly one letter you haven't already guessed"
+      return
+    end
+    true
+  end
+
+  def process_guess(guess)
+    if @secret_word.include?(guess)
+      @correct_guesses << guess
+    else
+      @incorrect_guesses << guess
+      @strike_counter += 1
+      puts "Strike #{@strike_counter}!"
+    end
   end
 end
